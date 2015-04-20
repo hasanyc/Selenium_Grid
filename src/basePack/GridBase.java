374d8875aeb1cd37188e99330fa478625cc1bae0
Hasan <hasanyc@hotmail.com>
2015-04-20 12:25:30 -0400

    By Hasan

--- src/basePack/GridBase.java
+++ src/basePack/GridBase.java
@@ -1,14 +1,12 @@
 package basePack;
 
 import java.net.MalformedURLException;
-<<<<<<< HEAD
-=======
 import java.net.URL;
->>>>>>> 16493d2d409f158039a9b8b788524de6d0ce9882
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -40,14 +38,14 @@ public class GridBase {
 			cap.setPlatform(Platform.WINDOWS);
 		}
 		// Remote WebDriver
-<<<<<<< HEAD
+
 		//driver = new RemoteWebDriver(new URL ("http://192.168.0.155:4444/wd/hub"), cap); // Local PC - HUB PC
 		//driver = new RemoteWebDriver(new URL ("http://192.168.0.227:5557/wd/hub"), cap);
 		driver = new FirefoxDriver();
-=======
+
 		driver = new RemoteWebDriver(new URL ("http://localhost:4444/wd/hub"), cap); // Local PC - HUB PC
 		//driver = new RemoteWebDriver(new URL ("http://remotePC-IP:NODE-#/wd/hub"), cap);
->>>>>>> 16493d2d409f158039a9b8b788524de6d0ce9882
+
 
 
 	}
@@ -64,20 +62,4 @@ public class GridBase {
 		driver.quit();
 	}
 
-<<<<<<< HEAD
-	//	@DataProvider(parallel=true, name="dataProvider")
-	//	public Object[][] getData() {
-	//		
-	//		Object data[][] = new Object[2][1];
-	//		// row 1
-	//		data[0][0]="chrome";
-	//		// row 2
-	//		data[1][0]="firefox";
-	//				
-	//		return data;
-	//		
-	//	}
-
-=======
->>>>>>> 16493d2d409f158039a9b8b788524de6d0ce9882
 }
